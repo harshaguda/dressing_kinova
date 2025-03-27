7af0d57aa32954b190a05ee8fe50702746d7bf66
Harsha Guda <harshavardhan.guda@gmail.com>
2025-03-27 11:06:39 +0000

    dressing using the model

--- src/dress_task.py
+++ src/dress_task.py
@@ -10,7 +10,7 @@ dpc = DeltaPoseControl()
 dummy_env = DummyEnv()
 ### Add code to use policy model
 
-model = PPO.load("model.zip", env=dummy_env, 
+model = PPO.load("/home/userlab/iri_lab/iri_ws/src/dressing_kinova/models/model (6).zip", env=dummy_env, 
          custom_objects={"policy_kwargs":
                          dict(
                              features_extractor_class=CustomCombinedExtractor,
@@ -18,7 +18,11 @@ model = PPO.load("model.zip", env=dummy_env,
                              activation_fn=torch.nn.modules.activation.Tanh
                          )}
          )
-
-dpc.set_cartesian_pose(0.1, 0, 0)
-dpc.set_cartesian_pose(0, 0.1, 0) 
-dpc.set_cartesian_pose(0, 0, -0.1)
+action_factor = 0.025
+obs = {}
+obs['hvert'] = torch.zeros(1, 9)
+obs['image'] = torch.zeros(1, 3, 214, 214)
+for i in range(2048):
+    action, _ = model.predict(obs)
+    x, y, z = action[0]*action_factor
+    dpc.set_cartesian_pose(x, y, z)
