26ece6fcc624bb09b5e6582340539d1f6e5a975d
Harsha Guda <harshavardhan.guda@gmail.com>
2025-04-03 15:12:17 +0200

    working

--- src/dress_task.py
+++ src/dress_task.py
@@ -10,9 +10,10 @@ import numpy as np
 import rospy
 
 def process_image(frame):
+    # frame = cv2.rotate(frame, cv2.ROTATE_180)
     resized_image = cv2.resize(frame, (214, 214))
-    image = np.swapaxes(resized_image, 0, 2)
-    return np.expand_dims(image, axis=0)
+    image = np.swapaxes(resized_image, 0, 2)/255.
+    return np.expand_dims(image, axis=0), resized_image
 
 dpc = DeltaPoseControl()
 dummy_env = DummyEnv()
@@ -30,14 +31,19 @@ action_factor = 0.025
 obs = {}
 obs['hvert'] = torch.zeros(1, 9)
 obs['image'] = torch.zeros(1, 3, 214, 214)
-cap = cv2.VideoCapture(4)
-
+cap = cv2.VideoCapture(5)
+base_action = torch.zeros(1, 3)
 flag, frame = cap.read()
 d = rospy.Duration(0.5)
 for i in range(2048):
     _, frame = cap.read()
-    obs['image'] = process_image(frame)
+    image_obs, res_img = process_image(frame)
+    obs['image'] = image_obs
+    obs['hvert'][:,-3:] = base_action
     action, _ = model.predict(obs)
-    x, y, z = action[0]*action_factor
+    x, y, z = -action[0]*action_factor
+    base_action += torch.tensor([x, y, z]).reshape(1,3)
     dpc.set_cartesian_pose(x, y, z)
+    # cv2.imshow('frame', res_img)
+    cv2.waitKey(1)
     rospy.sleep(d)
